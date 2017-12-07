0b6739e629402a4d63e3f1e9070915a8f1dd0016
宋健 <songjian@wikiflyer.cn>
2017-12-07 18:43:51 +0800

    pc端资讯

---
+++ app/controllers/site/articles_controller.rb
@@ -0,0 +1,11 @@
+class Site::ArticlesController < Site::BaseController
+
+  def index
+  end
+
+  def list
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  concern :list do
+    collection { get :list }
+  end
 
   root to: 'site/mains#show'
 
   scope module: :site do
+    resources :articles, only: [:index, :show], concerns: [:list]
   end
 end
