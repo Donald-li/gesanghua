140955fd84ffcd77d11454a3469439b043f8769a
宋健 <songjian@wikiflyer.cn>
2017-12-11 14:28:51 +0800

    配置simple_form

---
+++ db/schema.rb
@@ -0,0 +1,30 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20171211030517) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "administrators", force: :cascade, comment: "管理员" do |t|
+    t.string "login", comment: "登录名"
+    t.string "nickname", comment: "昵称"
+    t.string "password_digest", comment: "密码"
+    t.datetime "expire_at", default: "2099-12-31 00:00:00", comment: "过期时间"
+    t.integer "state", default: 1, comment: "状态 1:正常 2:禁用"
+    t.integer "kind", default: 2, comment: "管理员类型 1:超级管理员 2:普通管理员"
+    t.integer "integer", default: 2, comment: "管理员类型 1:超级管理员 2:普通管理员"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
