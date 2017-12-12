6977bb13ce37efdba8a477e0f6425c14680847b5
宋健 <songjian@wikiflyer.cn>
2017-12-12 16:52:15 +0800

    增加注释

--- spec/factories/adverts.rb
+++ spec/factories/adverts.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: adverts # 广告表
+#
+#  id            :integer          not null, primary key
+#  kind          :integer                                # 分类
+#  title         :string                                 # 标题
+#  description   :string                                 # 描述
+#  url           :string                                 # 链接
+#  views_count   :integer                                # 展示次数
+#  kind_position :integer                                # 分类排序
+#  state         :integer                                # 状态
+#  user_id       :integer                                # 用户id
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 FactoryBot.define do
   factory :advert do
     title '广告'
