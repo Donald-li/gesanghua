84b53d04b918a3afa2414c47c2ebbe6d44d159b3
宋健 <songjian@wikiflyer.cn>
2017-12-29 17:08:33 +0800

    财务分类设置

--- spec/models/finance_category_spec.rb
+++ spec/models/finance_category_spec.rb
@@ -2,15 +2,17 @@
 #
 # Table name: finance_categories # 财务分类表
 #
-#  id         :integer          not null, primary key
-#  name       :string                                 # 分类名称
-#  position   :integer                                # 排序
-#  fund_name  :string                                 # 基金名称
-#  amount     :decimal(14, 2)   default(0.0)          # 金额
-#  describe   :string                                 # 简介
-#  ancestry   :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string                                 # 分类名称
+#  position        :integer                                # 排序
+#  fund_name       :string                                 # 基金名称
+#  amount          :decimal(14, 2)   default(0.0)          # 金额
+#  total           :decimal(14, 2)   default(0.0)          # 历史收入
+#  management_rate :integer          default(0)            # 管理费率
+#  describe        :string                                 # 简介
+#  ancestry        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 
 require 'rails_helper'
