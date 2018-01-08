6e7ffe838b0ead09c8994b1eaef862e26b410eb0
xu baiheng <18363993647@163.com>
2018-01-08 10:14:07 +0800

    新增物资类项目 改名为 新增项目

--- spec/models/pair_spec.rb
+++ spec/models/pair_spec.rb
@@ -13,33 +13,6 @@
 #  updated_at :datetime         not null
 #
 
-<<<<<<< HEAD
-#  id                 :integer          not null, primary key
-#  type               :string                                 # 单表继承字段
-#  kind               :integer                                # 项目类型 1:固定项目 2:物资类项目
-#  name               :string                                 # 项目名称
-#  describe           :text                                   # 简介
-#  protocol           :text                                   # 用户协议
-#  fund               :integer                                # 关联财务分类id
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  junior_term_amount :decimal(14, 2)   default(0.0)          # 初中资助金额（学期）
-#  junior_year_amount :decimal(14, 2)   default(0.0)          # 初中资助金额（学年）
-#  senior_term_amount :decimal(14, 2)   default(0.0)          # 高中资助金额（学期）
-#  senior_year_amount :decimal(14, 2)   default(0.0)          # 高中资助金额（学年）
-=======
-#  id         :integer          not null, primary key
-#  type       :string                                 # 单表继承字段
-#  kind       :integer                                # 项目类型 1:固定项目 2:物资类项目
-#  name       :string                                 # 项目名称
-#  describe   :text                                   # 简介
-#  protocol   :text                                   # 用户协议
-#  fund_id    :integer                                # 关联财务分类id
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
->>>>>>> 554394f4b9ee31f1201ffe57283adc8e6eac2e58
-#
-
 require 'rails_helper'
 
 RSpec.describe Pair, type: :model do
