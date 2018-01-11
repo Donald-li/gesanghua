099c6cc20e51daccdb0a2d68a6bfdd2e45cd758c
董语 <dongyu@wikiflyer.cn>
2018-01-11 14:40:30 +0800

    v

--- app/models/project.rb
+++ app/models/project.rb
@@ -22,7 +22,7 @@ class Project < ApplicationRecord
   has_many :volunteer, class_name: 'Volunteer', dependent: :destroy
   has_many :donate_records
   has_many :teacher_projects
-  has_many :teachers, through: teacher_projects
+  has_many :teachers, through: :teacher_projects
 
   belongs_to :fund, optional: true
 
