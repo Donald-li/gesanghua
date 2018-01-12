f3e547923b8c64783c5003deac53be7837154584
宋健 <songjian@wikiflyer.cn>
2018-01-12 13:30:01 +0800

    去掉结对学期管理

--- config/routes.rb
+++ config/routes.rb
@@ -111,7 +111,6 @@ Rails.application.routes.draw do
 
     resources :pair_reports, concerns: [:switch]
     resources :pair_seasons, concerns: [:switch]
-    resources :pair_periods, concerns: [:switch, :move]
     resources :pair_donate_records, only: [:index, :show]
     resources :pair_applies do
       resources :pair_students, concerns: [:remarks] do
