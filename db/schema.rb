acabab3a69cdbcd0b34d07dd8411661258545bac
彭渤 <pengbo@wikiflyer.cn>
2018-01-16 17:43:36 +0800

    更新代码

--- db/schema.rb
+++ db/schema.rb
@@ -566,7 +566,7 @@ ActiveRecord::Schema.define(version: 20180116082554) do
     t.string "contact_phone", comment: "联系方式"
     t.string "contact_position", comment: "联系人职务"
     t.integer "kind", comment: "学校类型"
-    t.integer "user_id", comment: "用户id"
+    t.integer "user_id", comment: "申请人ID"
     t.string "school_no", comment: "学校申请编号"
     t.string "contact_idcard", comment: "联系人身份证号"
     t.string "postcode", comment: "邮政编码"
