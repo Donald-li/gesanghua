37dd25ad133ecab02731de0ce832d23830980e0c
宋健 <songjian@wikiflyer.cn>
2018-01-17 12:40:12 +0800

    删除测试中的pending

--- spec/helpers/admin/data_statistics_helper_spec.rb
+++ spec/helpers/admin/data_statistics_helper_spec.rb
@@ -11,5 +11,4 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe Admin::DataStatisticsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/child_trail_spec.rb
+++ spec/models/child_trail_spec.rb
@@ -15,5 +15,5 @@
 require 'rails_helper'
 
 RSpec.describe ChildTrail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
 end
