8f8b67cae952ba6085b0bbba966f8453203d74d9
宋健 <songjian@wikiflyer.cn>
2018-01-17 17:29:59 +0800

    用cancan替换pundit权限插件；项目介绍接口和测试

--- config/routes.rb
+++ config/routes.rb
@@ -210,6 +210,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :projects, only: :show
     end
   end
 
--- spec/controllers/admin/expenditure_records_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Admin::ExpenditureRecordsController, type: :controller do
-
-end
