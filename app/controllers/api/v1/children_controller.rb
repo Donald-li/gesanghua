0f081f7a807864327c6c201bd98dcf89ecbaf29e
xu baiheng <18363993647@163.com>
2018-01-19 14:08:42 +0800

    举报次数限制

--- app/controllers/api/v1/children_controller.rb
+++ app/controllers/api/v1/children_controller.rb
@@ -14,8 +14,12 @@ class Api::V1::ChildrenController < Api::V1::BaseController
 
   def complaint
     @complaint = Complaint.new(complaint_params)
-    @complaint.owner = ProjectSeasonApplyChild.find(params[:child_id])
-    if @complaint.save
+    @child = ProjectSeasonApplyChild.find(params[:child_id])
+    @complaint.owner = @child
+    complaint = Complaint.find_by(contact_phone: complaint_params[:contact_phone], owner: @child)
+    if complaint.present?
+      api_success(message: '您已经提交过举报信息，管理员会尽快处理')
+    elsif @complaint.save
       api_success(message: '举报成功，管理员会尽快处理')
     else
       api_success(message: '提交失败，请重试')
