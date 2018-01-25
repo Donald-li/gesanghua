0a6a09283ad26ba4b1cedbf82f016eee47c5fe36
xu baiheng <18363993647@163.com>
2018-01-25 12:12:01 +0800

    whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,7 +9,7 @@ set :output, "log/cron_log.log"
 # set :environment , :production
 set :environment , :development
 
-every 1.day, :at => '11:59 pm' do
+every 1.day, at: '11:50 pm' do
 # every 1.minute do
   runner "User.update_user_statistic_record"
   runner "IncomeRecord.update_income_statistic_record"
