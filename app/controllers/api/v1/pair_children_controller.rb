4f838ee349e0b577fe0f9fc1d050e72508d01a5d
xu baiheng <18363993647@163.com>
2018-01-29 10:50:07 +0800

    捐助记录创建问题

--- app/controllers/api/v1/pair_children_controller.rb
+++ app/controllers/api/v1/pair_children_controller.rb
@@ -39,7 +39,7 @@ class Api::V1::PairChildrenController < Api::V1::BaseController
     promoter = User.find(params[:promoter_id]) if params[:promoter_id].present?
     grants = GshChildGrant.where(id: params[:selected_grants].map {|grant| grant[:id]})
     grants.each do |grant|
-      record = DonateRecord.find_or_create_by(user: current_user, fund: @pair.project.fund, amount: grant[:amount].to_f, project: @pair.project, team: team, donor: params[:donor], remitter_id: current_user.id, remitter_name: current_user.name, season: @pair.season, apply: @pair.apply, project_season_apply_child: @pair)
+      record = DonateRecord.create(user: current_user, fund: @pair.project.fund, amount: grant[:amount].to_f, project: @pair.project, team: team, donor: params[:donor], remitter_id: current_user.id, remitter_name: current_user.name, season: @pair.season, apply: @pair.apply, project_season_apply_child: @pair)
       record.update(promoter_id: promoter.id) if promoter.present?
     end
     if params[:pay_method] == 'weixin' || params[:pay_method] == 'balance_and_weixin'
