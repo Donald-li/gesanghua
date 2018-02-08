751926a963467e98ce738aa21b38f3d1b5abd09a
宋健 <songjian@wikiflyer.cn>
2018-02-08 18:55:12 +0800

    优化代捐人

--- app/controllers/api/v1/offline_donors_controller.rb
+++ app/controllers/api/v1/offline_donors_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::OfflineDonorsController < Api::V1::BaseController
 
   def donor_list
-    donors = current_user.offline_donors
+    donors = current_user.offline_users
     api_success(data: donors.map{|donor| donor.offline_donor_summary_builder})
   end
 
