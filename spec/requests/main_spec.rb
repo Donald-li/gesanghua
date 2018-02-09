16d02ba05c9ac7b262cd9e305011dc3760a17d73
董语 <dongyu@wikiflyer.cn>
2018-02-09 17:08:09 +0800

    修改

--- spec/requests/main_spec.rb
+++ spec/requests/main_spec.rb
@@ -26,25 +26,6 @@ RSpec.describe "Api::V1::Main", type: :request do
   end
 
   describe '单次捐助' do
-    # it '捐助给格桑花' do
-    #   post settlement_api_v1_main_path,
-    #        params: {amount: '5000', project: {"name"=>"格桑花", "value"=>"toGsh"},
-    #                 donor_name: '好心人', by_team: false, pay_method: 'weixin'},
-    #        headers: api_v1_headers(login_user)
-    #   api_v1_expect_success
-    #   expect(json_body[:data][:record_state]).to eq true
-    # end
-    #
-    # it '捐助给指定项目' do
-    #   create(:project)
-    #   post settlement_api_v1_main_path,
-    #        params: {amount: '5000', project: {"name"=>"一对一", "value"=>"1"},
-    #                 donor_name: '好心人', by_team: false, pay_method: 'weixin'},
-    #        headers: api_v1_headers(login_user)
-    #   api_v1_expect_success
-    #   expect(json_body[:data][:record_state]).to eq true
-    # end
-    
     it '捐助给所选可捐助项目' do
       post settlement_api_v1_main_path,
            params: {amount: '5000', donation_project: {"name"=>"一对一", "value"=>"1"},
@@ -56,25 +37,6 @@ RSpec.describe "Api::V1::Main", type: :request do
   end
 
   describe '月捐' do
-    # it '捐助给格桑花' do
-    #   post settlement_api_v1_main_path,
-    #        params: {amount: '5000', project: {"name"=>"格桑花", "value"=>"toGsh"},
-    #                 period: '3', pay_method: 'weixin'},
-    #        headers: api_v1_headers(login_user)
-    #   api_v1_expect_success
-    #   expect(json_body[:data][:record_state]).to eq true
-    # end
-    #
-    # it '捐助给指定项目' do
-    #   create(:project)
-    #   post settlement_api_v1_main_path,
-    #        params: {amount: '5000', project: {"name"=>"一对一", "value"=>"1"},
-    #                 period: '3', pay_method: 'weixin'},
-    #        headers: api_v1_headers(login_user)
-    #   api_v1_expect_success
-    #   expect(json_body[:data][:record_state]).to eq true
-    # end
-
     it '捐助给所选可捐助项目' do
       post settlement_api_v1_main_path,
            params: {amount: '5000', donation_project: {"name"=>"一对一", "value"=>"1"},
