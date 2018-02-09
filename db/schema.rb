15e5377a2d9e77a6ab235ad7e2126791cf4f0a68
董语 <dongyu@wikiflyer.cn>
2018-02-09 09:41:35 +0800

    小优化

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180208100809) do
-=======
 ActiveRecord::Schema.define(version: 20180208112619) do
->>>>>>> 优化代码
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
