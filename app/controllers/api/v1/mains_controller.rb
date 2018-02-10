8cb60540baf0c5a1331162c695d5d33c91b13939
董语 <dongyu@wikiflyer.cn>
2018-02-10 17:52:10 +0800

    修改

--- app/controllers/api/v1/mains_controller.rb
+++ app/controllers/api/v1/mains_controller.rb
@@ -36,7 +36,7 @@ class Api::V1::MainsController < Api::V1::BaseController
   def month_contribute
     amount = params[:amount].to_f
     plan_period = params[:period].to_i
-    donate_item = Donation.find_donate_item(params[:donate_item][:value])
+    donate_item = Donation.find(params[:donate_item][:value])
     record = MonthDonate.month_donate_donate_item(current_user, amount, donate_item, plan_period)
     if record.save
       api_success(data: {record_state: true, record_id: record.id, user_info: current_user.summary_builder.merge(auth_token: current_user.auth_token)}, message: '订单生成成功（暂时提示，应调用微信支付捐助成功后跳转结果页）')
