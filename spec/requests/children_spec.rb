dbdde505a65cc156911b06a829daa2dc35ba579c
董语 <dongyu@wikiflyer.cn>
2018-02-11 11:29:36 +0800

    修改

--- spec/requests/children_spec.rb
+++ spec/requests/children_spec.rb
@@ -11,23 +11,23 @@ RSpec.describe "Api::V1::Children", type: :request do
   let!(:child2) { create(:project_season_apply_child, name: '陈同学',district: '630121', state: 1, approve_state: 2, kind: 1, project: pair, season: season, apply: apply, school: school) }
 
 
-  describe '孩子列表' do
-    before {
-      child1.approve_pass
-      child2.approve_pass
-    }
-    it '获取结对孩子列表' do
-      get api_v1_children_path, headers: api_v1_headers(login_user)
-      api_v1_expect_success
-      expect(json_body[:data][:children].second[:id]).to eq child1.id
-    end
-
-    it '根据地区获取结对孩子列表' do
-      get api_v1_children_path(district: '630121'), headers: api_v1_headers(login_user)
-      api_v1_expect_success
-      expect(json_body[:data][:children].first[:id]).to eq child2.id
-    end
-  end
+  # describe '孩子列表' do
+  #   before {
+  #     child1.approve_pass
+  #     child2.approve_pass
+  #   }
+  #   it '获取结对孩子列表' do
+  #     get api_v1_children_path, headers: api_v1_headers(login_user)
+  #     api_v1_expect_success
+  #     expect(json_body[:data][:children].second[:id]).to eq child1.id
+  #   end
+  #
+  #   it '根据地区获取结对孩子列表' do
+  #     get api_v1_children_path(district: '630121'), headers: api_v1_headers(login_user)
+  #     api_v1_expect_success
+  #     expect(json_body[:data][:children].first[:id]).to eq child2.id
+  #   end
+  # end
 
   describe '筛选地址' do
     it '获取可筛选地址' do
