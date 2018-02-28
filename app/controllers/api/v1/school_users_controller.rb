851c6fc920da2c8d5466d7a0198e350c389b0cda
董语 <dongyu@wikiflyer.cn>
2018-02-28 22:06:00 +0800

    学校用户教师管理

--- app/controllers/api/v1/school_users_controller.rb
+++ app/controllers/api/v1/school_users_controller.rb
@@ -40,7 +40,87 @@ class Api::V1::SchoolUsersController < Api::V1::BaseController
     else
       api_success(data: {is_school_user: false}, message: '您不是学校用户！')
     end
-
   end
 
+  def get_school_teachers
+    @user = current_user
+    @school = @user.school if @user.present?
+    @teachers = @school.teachers
+    api_success(data: @teachers.map{|teacher| teacher.summary_builder})
+  end
+
+  def create_teacher
+    @user = current_user
+    @school = @user.school if @user.present?
+    if @school.present?
+      name = params[:name]
+      phone = params[:phone]
+      idcard = params[:idcard]
+      qq = params[:qq]
+      openid = params[:openid]
+      teacher_projects = params[:teacher_projects]
+      @teacher = @school.teachers.new(name: name, phone: phone, idcard: idcard, qq: qq, openid: openid)
+      # @teacher.new(name: name, phone: phone, idcard: idcard, qq: qq, openid: openid)
+      if @teacher.save
+        teacher_projects.each do |teacher_project|
+          TeacherProject.create(project_id: teacher_project, teacher: @teacher)
+        end
+        api_success(data: {is_school_user: true, result: true}, message: '新建教师信息成功。')
+      else
+        api_success(data: {is_school_user: true, result: false}, message: '新建教师信息失败，请重试！')
+      end
+    else
+      api_success(data: {is_school_user: false}, message: '您不是学校用户！')
+    end
+  end
+
+  def get_teacher
+    @teacher = Teacher.find(params[:id])
+    api_success(data: @teacher.detail_builder)
+  end
+
+  def get_projects
+    @projects = Project.all
+    api_success(data: @projects.map{|project| project.teacher_project_summary_builder})
+  end
+
+  def update_teacher
+    @user = current_user
+    @school = @user.school if @user.present?
+    if @school.present?
+      name = params[:name]
+      phone = params[:phone]
+      idcard = params[:idcard]
+      qq = params[:qq]
+      openid = params[:openid]
+      teacher_projects = params[:teacher_projects]
+      @teacher = Teacher.find(params[:id])
+      if @teacher.update(name: name, phone: phone, idcard: idcard, qq: qq, openid: openid)
+        TeacherProject.where(teacher: @teacher).destroy_all
+        teacher_projects.each do |teacher_project|
+          TeacherProject.create(project_id: teacher_project, teacher: @teacher)
+        end
+        api_success(data: {is_school_user: true, result: true}, message: '修改教师信息成功。')
+      else
+        api_success(data: {is_school_user: true, result: false}, message: '修改教师信息失败，请重试！')
+      end
+    else
+      api_success(data: {is_school_user: false}, message: '您不是学校用户！')
+    end
+  end
+
+  def delete_teacher
+    @user = current_user
+    @school = @user.school if @user.present?
+    if @school.present?
+      @teacher = @school.teachers.find(params[:id])
+      if @teacher.destroy
+        api_success(data: {is_school_user: true, result: true}, message: '删除教师信息成功。')
+      else
+        api_success(data: {is_school_user: true, result: false}, message: '删除教师信息失败，请重试！')
+      end
+    else
+      api_success(data: {is_school_user: false}, message: '您不是学校用户！')
+    end
+  end
 end
