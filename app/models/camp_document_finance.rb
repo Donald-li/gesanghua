319468bc7f7f9d5b3016394cdcfe9da0b92c41c1
xu baiheng <18363993647@163.com>
2018-03-05 10:57:14 +0800

    数据迁移

--- app/models/camp_document_finance.rb
+++ app/models/camp_document_finance.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: camp_document_finances # 拓展营预决算表
+#
+#  id         :integer          not null, primary key
+#  project_id :integer                                # 项目
+#  user_id    :integer                                # 用户
+#  item       :string                                 # 项
+#  budge      :decimal(14, 2)   default(0.0)          # 预算
+#  amount     :decimal(14, 2)   default(0.0)          # 决算
+#  remark     :string                                 # 备注
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class CampDocumentFinance < ApplicationRecord
 end
