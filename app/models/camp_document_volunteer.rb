4c45761a9381891b48859a462fceb718d4387944
xu baiheng <18363993647@163.com>
2018-03-05 13:40:22 +0800

    annotate

--- app/models/camp_document_volunteer.rb
+++ app/models/camp_document_volunteer.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: camp_document_volunteers # 拓展营志愿者表
+#
+#  id           :integer          not null, primary key
+#  project_id   :integer                                # 项目
+#  user_id      :integer                                # 用户
+#  volunteer_id :integer                                # 志愿者
+#  remark       :string                                 # 营备注
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class CampDocumentVolunteer < ApplicationRecord
 end
--- spec/models/camp_document_volunteer_spec.rb
+++ spec/models/camp_document_volunteer_spec.rb
@@ -1,5 +1,18 @@
+# == Schema Information
+#
+# Table name: camp_document_volunteers # 拓展营志愿者表
+#
+#  id           :integer          not null, primary key
+#  project_id   :integer                                # 项目
+#  user_id      :integer                                # 用户
+#  volunteer_id :integer                                # 志愿者
+#  remark       :string                                 # 营备注
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe CampDocumentVolunteer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
