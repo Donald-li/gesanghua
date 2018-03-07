cad9e94bbd04d2339068ad8007158b8d6f2954d8
董语 <dongyu@wikiflyer.cn>
2018-03-07 17:45:03 +0800

    收货安装发放反馈

--- app/controllers/api/v1/install_feedbacks_controller.rb
+++ app/controllers/api/v1/install_feedbacks_controller.rb
@@ -6,9 +6,19 @@ class Api::V1::InstallFeedbacksController < Api::V1::BaseController
     content = params[:content]
     images = params[:images]
     @apply = ProjectSeasonApply.find(params[:apply_id])
-    if params[:bookshelf_id].present?
+    if params[:bookshelf_supplement_id].present?
+      bookshelf_supplement_id = params[:bookshelf_supplement_id]
+      @bookshelf_supplement = BookshelfSupplement.find(bookshelf_supplement_id)
+      @install = InstallFeedback.new(content: content, project_season_apply_id: apply_id, project_id: @bookshelf_supplement.apply.project.id, project_season_id: @bookshelf_supplement.apply.season.id, project_season_apply_bookshelf_id: @bookshelf_supplement.bookshelf.id, user_id: @user.id, owner: @bookshelf_supplement)
+      if @install.save
+        @install.attach_images(params[:images].map{|image| image[:id]}) if params[:images].present?
+        api_success(data: {result: true}, message: '您的反馈已提交～')
+      else
+        api_success(data: {result: false}, message: '反馈提交失败，请重试！')
+      end
+    elsif params[:bookshelf_id].present?
       bookshelf_id = params[:bookshelf_id]
-      @bookshelf = ProjectSeasonApplyBookshelf.find(params[:bookshelf_id])
+      @bookshelf = ProjectSeasonApplyBookshelf.find(bookshelf_id)
       @install = InstallFeedback.new(content: content, project_season_apply_id: apply_id, project_id: @bookshelf.apply.project.id, project_season_id: @bookshelf.apply.season.id, project_season_apply_bookshelf_id: bookshelf_id, user_id: @user.id, owner: @bookshelf)
       if @install.save
         @install.attach_images(params[:images].map{|image| image[:id]}) if params[:images].present?
