d326e5d579ec5e4056e8459200f41dcd6baf506b
宋健 <songjian@wikiflyer.cn>
2018-03-08 20:39:37 +0800

    优化捐助记录接口

--- app/controllers/api/v1/donate_records_controller.rb
+++ app/controllers/api/v1/donate_records_controller.rb
@@ -4,12 +4,10 @@ class Api::V1::DonateRecordsController < Api::V1::BaseController
   def index
     if params[:type] == 'child'
       donate_records = DonateRecord.where(project_season_apply_child_id: params[:item_id]).sorted.page(params[:page]).per(7)
-    # elsif params[:type] == 'project' ?
-    #   donate_records = DonateRecord.where(project_id: params[:item_id]).sorted.page(params[:page]).per(7)
     elsif params[:type] == 'teamDonate'
       donate_records = DonateRecord.where(team_id: params[:item_id]).sorted.page(params[:page]).per(7)
-    elsif params[:type] == 'read'
-      donate_records = DonateRecord.where(project_season_apply_id: params[:item_id]).paid.sorted.page(params[:page]).per(7)
+    elsif params[:project_id].present?
+      donate_records = DonateRecord.where(project_id: params[:project_id]).paid.sorted.page(params[:page]).per(7)
     else
       donate_records = DonateRecord.sorted.page(params[:page]).per(7)
     end
