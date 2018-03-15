32c297e9a0309e192365be26f92357e5b6a3a4af
董语 <dongyu@wikiflyer.cn>
2018-03-15 16:46:32 +0800

    一对一学生详情页图片显示问题

--- app/uploaders/apply_child_avatar_uploader.rb
+++ app/uploaders/apply_child_avatar_uploader.rb
@@ -8,9 +8,9 @@ class ApplyChildAvatarUploader < BaseUploader
   # version :tiny do
   #   process :resize_to_fill => [1014, 608]
   # end
-  # version :small do
-  #   process :resize_to_fill => [130, 130]
-  # end
+  version :small do
+    process :resize_to_fill => [130, 130]
+  end
   # version :medium do
   #   process :resize_to_fill => [360, 360]
   # end
