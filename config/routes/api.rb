323c45ce676b843a8d053a06ad5cd46d26180f87
董语 <dongyu@wikiflyer.cn>
2018-03-15 17:14:01 +0800

    修改

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -66,7 +66,6 @@ namespace :api do
       resources :mains, only: [:index]
       resources :schools, only: [:create]
       resources :project_applies, only: [:new, :show] # 项目申请
-      resources :read_project_applies, only: [:new, :create] # 悦读项目申请
 
       resources :gsh_children, only: [] do
         collection do
