7a32438d14d94c5ed1d074b2bdadff3dc9f59362
xu baiheng <18363993647@163.com>
2018-03-16 18:19:39 +0800

    删除方法

--- app/models/month_donate.rb
+++ app/models/month_donate.rb
@@ -62,16 +62,4 @@ class MonthDonate < ApplicationRecord
     return donate
   end
 
-  def certificate_builder
-    Jbuilder.new do |json|
-      json.(self, :id)
-      json.donate_item_name self.donate_item.try(:name)
-      json.amount number_to_currency(self.amount)
-      json.user_name self.user.present? ? self.user.name : '爱心人士'
-      json.time_name "#{self.created_at.year}年#{self.created_at.month}月#{self.created_at.day}日"
-      json.certificate_no self.certificate_no
-      json.project self.try(:project).try(:name)
-    end.attributes!
-  end
-
 end
