4164f4717af5ac834a45fd926f81aa7234015134
董语 <dongyu@wikiflyer.cn>
2018-03-21 20:16:23 +0800

    修复反馈内容不填写的提示

--- app/controllers/api/v1/cooperation_regular_feedbacks_controller.rb
+++ app/controllers/api/v1/cooperation_regular_feedbacks_controller.rb
@@ -12,12 +12,12 @@ class Api::V1::CooperationRegularFeedbacksController < Api::V1::BaseController
       feedback.attach_images(params[:images].pluck(:id)) if params[:images].present?
       api_success(data: true, message: '你的反馈已提交~')
     else
-      api_success(data: false, message: '反馈失败，请重试~')
+      api_success(data: false, message: feedback.errors.full_messages.first)
     end
   end
 
   def feedback_list
-    feedbacks =  @project.continual_feedbacks.page(params[:page]).per(params[:per])
+    feedbacks =  @project.continual_feedbacks.sorted.page(params[:page]).per(params[:per])
     api_success(data: {feedbacks: feedbacks.map{|f| f.detail_builder}, pagination: json_pagination(feedbacks)})
   end
 
