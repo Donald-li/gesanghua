e13ebb9e46918fb60961b7fa6009af17662d97c2
xu baiheng <18363993647@163.com>
2018-03-26 18:49:58 +0800

    创建团队逻辑

--- app/controllers/api/v1/account/teams_controller.rb
+++ app/controllers/api/v1/account/teams_controller.rb
@@ -9,15 +9,15 @@ class Api::V1::Account::TeamsController < Api::V1::BaseController
   end
 
   def create
-    if current_user.team.present?
-    team = Team.new(team_params.merge(province: params[:location][0], city: params[:location][1], district: params[:location][2], kind: params[:kind], creater_id: current_user.id, manage_id: current_user.id))
-    if team.save
-      team.attach_logo(params[:logo_id])
-      current_user.update(team: team, join_team_time: Time.now)
-      api_success(message: '创建成功', data: true)
-    else
-      api_success(message: '创建失败，请重试', data: false)
-    end
+    unless current_user.team.present?
+      team = Team.new(team_params.merge(province: params[:location][0], city: params[:location][1], district: params[:location][2], kind: params[:kind], creater_id: current_user.id, manage_id: current_user.id))
+      if team.save
+        team.attach_logo(params[:logo_id])
+        current_user.update(team: team, join_team_time: Time.now)
+        api_success(message: '创建成功', data: true)
+      else
+        api_success(message: '创建失败，请重试', data: false)
+      end
     else
       api_success(message: '创建失败，您已经有自己的团队了', data: false)
     end
