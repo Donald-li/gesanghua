9c6edc8a88515756dda06936866e2840fd784e8f
xu baiheng <18363993647@163.com>
2018-03-28 16:22:16 +0800

    测试account/user

--- app/controllers/api/v1/account/my_reads_controller.rb
+++ app/controllers/api/v1/account/my_reads_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Account::MyReadsController < Api::V1::BaseController
 
   def index
     # donates =  DonateRecord.joins(:bookshelf).where(user_id: 2).where("project_season_apply_bookshelves.target_amount = donate_records.amount")
-    user_id = 2
+    user_id = current_user.id
     scope = ProjectSeasonApplyBookshelf.joins(:donates).where("donate_records.amount = project_season_apply_bookshelves.target_amount").where("donate_records.user_id = ? and donate_records.pay_state = ?", user_id, 2)
     bookshelves = scope.page(params[:page]).per(params[:per])
     api_success(data: {bookshelves: bookshelves.map { |r| r.summary_builder }, pagination: json_pagination(bookshelves)})
