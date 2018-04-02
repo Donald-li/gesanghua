fb77ab0f19d5cfcae7f5842dc9068c2648c1701a
xu baiheng <18363993647@163.com>
2018-04-02 17:02:03 +0800

    整理表结构

--- db/migrate/20180330073148_add_column_teacher_id_to_project_season_apply_camp.rb
+++ db/migrate/20180330073148_add_column_teacher_id_to_project_season_apply_camp.rb
@@ -1,7 +1,5 @@
 class AddColumnTeacherIdToProjectSeasonApplyCamp < ActiveRecord::Migration[5.1]
   def change
     add_column :project_season_apply_camps, :teacher_id, :integer, comment: '联系老师id'
-    add_column :project_season_apply_camp_students, :age, :integer, comment: '年龄'
-    add_column :project_season_apply_camp_teachers, :age, :integer, comment: '年龄'
   end
 end
