d8cb446f3c7a91ea3934a72a63bfd0baabb541ea
xu baiheng <18363993647@163.com>
2018-04-02 17:48:47 +0800

    探索营成员管理优化

--- app/controllers/admin/project_season_apply_camp_students_controller.rb
+++ app/controllers/admin/project_season_apply_camp_students_controller.rb
@@ -3,7 +3,7 @@ class Admin::ProjectSeasonApplyCampStudentsController < Admin::BaseController
   before_action :set_apply_camp
 
   def index
-    @search = ProjectSeasonApplyCampMember.student.sorted.ransack(params[:q])
+    @search = @apply_camp.project_season_apply_camp_members.student.sorted.ransack(params[:q])
     scope = @search.result
     @camp_students = scope.page(params[:page])
   end
--- app/controllers/admin/project_season_apply_camp_teachers_controller.rb
+++ app/controllers/admin/project_season_apply_camp_teachers_controller.rb
@@ -3,7 +3,7 @@ class Admin::ProjectSeasonApplyCampTeachersController < Admin::BaseController
   before_action :set_apply_camp
 
   def index
-    @search = ProjectSeasonApplyCampMember.teacher.sorted.ransack(params[:q])
+    @search = @apply_camp.project_season_apply_camp_members.teacher.sorted.ransack(params[:q])
     scope = @search.result
     @camp_teachers = scope.page(params[:page])
   end
