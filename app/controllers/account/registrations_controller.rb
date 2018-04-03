cff00bce03cc4cfa853d407fe690b4fd75df9dfe
董语 <dongyu@wikiflyer.cn>
2018-04-03 12:33:48 +0800

    用手机注册

--- app/controllers/account/registrations_controller.rb
+++ app/controllers/account/registrations_controller.rb
@@ -8,32 +8,34 @@ class Account::RegistrationsController < Account::BaseController
   end
 
   def create
-    # @user = User.new(user_params)
+    @user = User.new(user_params)
     respond_to do |format|
-      # format.html {redirect_to new_account_registration_url, alert: '验证码不能为空。'}
-      # format.html {redirect_to new_account_registration_url, notice: '验证码不能为空。'}
-      format.html {
-        flash[:notice] = '验证码不能为空。'
-        render 'new'
-      }
-      # if params[:code].empty?
-      #   format.html {redirect_to new_account_registration_url, alert: '验证码不能为空。'}
-      # end
-      #
-      # unless SmsCode.valid_code?(mobile: user_params[:login], code: params[:code].to_i)
-      #   format.html {redirect_to new_account_registration_url, alert: '验证码错误。'}
-      # end
-      #
-      # if User.find_by(login: user_params[:login])
-      #   format.html {redirect_to new_account_registration_url, alert: '手机号已注册。'}
-      # else
-      #   if @user.save
-      #     set_current_user(@user)
-      #     format.html { redirect_to account_registration_path, article: '注册成功。' }
-      #   else
-      #     format.html { render :new }
-      #   end
-      # end
+      if User.find_by(login: user_params[:login])
+        format.html {redirect_to new_account_registration_url(kind: params[:kind]), alert: params[:kind] == 'by_phone'? '手机号已注册。' : '邮箱已注册。'}
+      else
+        if params[:check] != 'on'
+          format.html {redirect_to new_account_registration_url(kind: params[:kind]), alert: '请同意《格桑花用户注册协议》。'}
+        end
+        if params[:code].empty?
+          format.html {redirect_to new_account_registration_url(kind: params[:kind]), alert: '验证码不能为空。'}
+        end
+
+        unless SmsCode.valid_code?(mobile: user_params[:login], code: params[:code], kind: 'signup')
+          format.html {redirect_to new_account_registration_url(kind: params[:kind]), alert: '验证码错误。'}
+        end
+        @user.name = user_params[:login]
+        if params[:kind] == 'by_phone'
+          @user.phone = user_params[:login]
+        else
+          @user.email = user_params[:login]
+        end
+        if @user.save
+          set_current_user(@user)
+          format.html { redirect_to root_path, user: '注册成功。' }
+        else
+          format.html {redirect_to new_account_registration_url(kind: params[:kind]), alert: @user.errors.full_messages}
+        end
+      end
     end
   end
 
