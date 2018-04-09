391b45686e9ebb8bba1f7e4c9c15c83706db969f
王宪伟 <wangxw@wikiflyer.cn>
2018-04-09 19:02:50 +0800

    百度接口

--- spec/lib/baidu_spec.rb
+++ spec/lib/baidu_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe Baidu, type: :lib do
 
   it '测试签名' do
-    token = Baidu.auth_str(url: 'https://bos.cn-n1.baidubce.com/example?text&text1=测试&text10=test', headers: {Auth: 'aaa'})
+    # token = Baidu.auth_str(url: 'https://bos.cn-n1.baidubce.com/example?text&text1=测试&text10=test', headers: {Auth: 'aaa'})
     # pp token
   end
 
   it '测试发短信' do
-    res = Baidu.send_sms
-    # pp res.body
+    data = Baidu.send_sms
+    pp data
+
+    # data = Baidu.send_test
+    # pp data
   end
 end
