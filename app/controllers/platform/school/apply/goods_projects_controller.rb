4662bd5a4c29e2ccce9df16aa77385a77e213501
王宪伟 <wangxw@wikiflyer.cn>
2018-04-12 19:30:31 +0800

    调试了一下问题

--- app/controllers/platform/school/apply/goods_projects_controller.rb
+++ app/controllers/platform/school/apply/goods_projects_controller.rb
@@ -1,4 +1,4 @@
-class Platform::School::Apply::GoodsController < Platform::School::BaseController
+class Platform::School::Apply::GoodsProjectsController < Platform::School::BaseController
   before_action :set_goods_project
   def index
   end
