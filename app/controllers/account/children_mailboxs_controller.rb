f7456a34c1b48ab031856a2b6aa22b367e7dc371
董语 <dongyu@wikiflyer.cn>
2018-04-13 16:32:51 +0800

    我的结对

--- app/controllers/account/children_mailboxs_controller.rb
+++ app/controllers/account/children_mailboxs_controller.rb
@@ -1,3 +1,9 @@
 class Account::ChildrenMailboxsController < Account::BaseController
+  
+  def index
+    @child = ProjectSeasonApplyChild.find(params[:pair_id])
+    scope = ContinualFeedback.where(gsh_child_grant_id: @child.gsh_child_grants.where(user_id: current_user.id).pluck(:id)).sorted
+    @feedbacks = scope.page(params[:page]).per(4)
+  end
 
 end
--- app/controllers/account/pairs_controller.rb
+++ app/controllers/account/pairs_controller.rb
@@ -1,3 +1,8 @@
 class Account::PairsController < Account::BaseController
 
+  def index
+    scope = ProjectSeasonApplyChild.where(id: current_user.gsh_child_grants.pluck(:gsh_child_id)).sorted
+    @children = scope.page(params[:page]).per(4)
+  end
+
 end
