76d4b9820d60420f4253377e4e7ba02a6e96299c
mqh <mengqh@wikiflyer.cn>
2018-04-13 17:06:30 +0800

    教师管理

--- app/controllers/platform/school/teachers_controller.rb
+++ app/controllers/platform/school/teachers_controller.rb
@@ -3,7 +3,7 @@ class Platform::School::TeachersController < Platform::School::BaseController
   before_action :set_teacher, only: [:edit, :update, :destroy]
 
   def index
-    @teachers = Teacher.includes(:projects).sorted.page(params[:page]).per(1)
+    @teachers = Teacher.includes(:projects).sorted.page(params[:page]).per(10)
     respond_to do |format|
       format.html
       format.js
@@ -15,13 +15,13 @@ class Platform::School::TeachersController < Platform::School::BaseController
   end
 
   def create
-    @teacher = Teacher.new(teacher_params)
-    @teacher.attach_avatar(params[:avatar_id])
+    @teacher = Teacher.new(teacher_params.merge(school: current_user.school))
+    #@teacher.attach_avatar(params[:avatar_id])
     respond_to do |format|
       if @teacher.save
         format.html { redirect_to platform_school_teachers_path, notice: '创建成功。' }
       else
-        format.html { render :new }
+        format.html { render :new, notice: @teacher.errors.full_messages }
       end
     end
   end
@@ -31,21 +31,20 @@ class Platform::School::TeachersController < Platform::School::BaseController
   end
 
   def update
-    binding.pry
-    #@teacher.project_ids = params[:teacher][:project_ids]
-    # if @teacher.update(teacher_params)
-    #   @teacher.attach_avatar(params[:avatar_id])
-    #   format.html { redirect_to platform_school_teachers_path, notice: '教师已修改。' }
-    # else
-    #   format.html { render :edit }
-    # end
+    @teacher.project_ids = params[:teacher][:project_ids]
+    if @teacher.update(teacher_params)
+      #@teacher.attach_avatar(params[:avatar_id])
+      format.html { redirect_to platform_school_teachers_path, notice: '教师已修改。' }
+    else
+      format.html { render :edit, notice: @teacher.errors.full_messages }
+    end
   end
 
   def destroy
-    # @teacher.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to platform_school_teachers_path, notice: '删除成功。' }
-    # end
+    @teacher.destroy
+    respond_to do |format|
+      format.html { redirect_to platform_school_teachers_path, notice: '删除成功。' }
+    end
   end
 
   private
