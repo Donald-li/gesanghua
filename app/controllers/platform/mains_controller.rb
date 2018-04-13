965a0e3b9dafe10428edfc007843de58a387b001
董语 <dongyu@wikiflyer.cn>
2018-04-13 17:23:57 +0800

    协作平台

--- app/controllers/platform/mains_controller.rb
+++ app/controllers/platform/mains_controller.rb
@@ -2,7 +2,7 @@ class Platform::MainsController < Platform::School::BaseController
   layout 'account'
 
   def show
-    @regular_feedback_url = "#{Settings.m_root_url}/cooperation/pair-feedback"
+    @regular_feedback_url = "#{Settings.m_root_url}/cooperation"
   end
 
 end
