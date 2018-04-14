b79e3d8f2e29bf512ef95a9e84a50708665e7110
宋健 <songjian@wikiflyer.cn>
2018-04-14 10:15:10 +0800

    我的捐助接口

--- app/controllers/api/v1/account/donate_records_controller.rb
+++ app/controllers/api/v1/account/donate_records_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::Account::DonateRecordsController < Api::V1::BaseController
 
   def index
-    donate_records = current_user.donate_records.where.not(project_id: nil).sorted.page(params[:page]).per(params[:per])
-    donate_records = donate_records.where(project_id: params[:project_id].to_i) if params[:project_id].present? && params[:project_id] != 'all'
-    api_success(data: {donate_records: donate_records.map { |r| r.detail_builder }, pagination: json_pagination(donate_records)})
+    donations = current_user.donations.where.not(project_id: nil).sorted.page(params[:page]).per(params[:per])
+    donations = donations.where(project_id: params[:project_id].to_i) if params[:project_id].present? && params[:project_id] != 'all'
+    api_success(data: {donate_records: donations.map { |r| r.detail_builder }, pagination: json_pagination(donations)})
   end
 
   def projects
