755f03da4ad490fc1b589ce5bcc7faaec029c6ac
mqh <mengqh@wikiflyer.cn>
2018-04-17 18:01:16 +0800

    翻页

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  scope :decode_page, ->(params) { page(params[:page]).per(params[:per] || 20) }
+
   def self.all_to_hash
     self.all.map{|c| [c.name, c.id]}
   end
