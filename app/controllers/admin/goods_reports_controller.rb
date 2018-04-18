d28bc53ea4a12525302c101a072ba2258003f3bd
董语 <dongyu@wikiflyer.cn>
2018-04-18 10:25:14 +0800

    悦读和物资类项目报告

--- app/controllers/admin/goods_reports_controller.rb
+++ app/controllers/admin/goods_reports_controller.rb
@@ -1,11 +1,10 @@
-class Admin::GoodsReportsController < Admin::BaseController
+class Admin::GoodsReportsController < Admin::GoodsBaseController
   before_action :check_auth
   before_action :set_report, only: [:edit, :update, :destroy, :switch]
-  before_action :set_project
 
   def index
     set_search_end_of_day(:published_at_lteq)
-    @search = ProjectReport.where(project_id: @project.id).sorted.ransack(params[:q])
+    @search = ProjectReport.where(project_id: @current_project.id).sorted.ransack(params[:q])
     scope = @search.result
     @reports = scope.page(params[:page])
   end
@@ -18,11 +17,11 @@ class Admin::GoodsReportsController < Admin::BaseController
   end
 
   def create
-    @report = ProjectReport.new(report_params.merge(project_id: @project.id, user_id: current_user.id))
+    @report = ProjectReport.new(report_params.merge(project_id: @current_project.id, user_id: current_user.id))
     @report.attach_images(params[:image_ids])
     respond_to do |format|
       if @report.save
-        format.html { redirect_to referer_or(admin_goods_reports_url(pid: @project.id)), notice: '报告已增加。' }
+        format.html { redirect_to referer_or(admin_goods_reports_url(pid: @current_project.id)), notice: '报告已增加。' }
       else
         format.html { render :new }
       end
@@ -33,7 +32,7 @@ class Admin::GoodsReportsController < Admin::BaseController
     @report.attach_images(params[:image_ids])
     respond_to do |format|
       if @report.update(report_params)
-        format.html { redirect_to referer_or(admin_goods_reports_url(pid: @project.id)), notice: '报告已修改。' }
+        format.html { redirect_to referer_or(admin_goods_reports_url(pid: @current_project.id)), notice: '报告已修改。' }
       else
         format.html { render :edit }
       end
@@ -43,20 +42,16 @@ class Admin::GoodsReportsController < Admin::BaseController
   def destroy
     @report.destroy
     respond_to do |format|
-      format.html { redirect_to referer_or(admin_goods_reports_url(pid: @project.id)), notice: '报告已删除。' }
+      format.html { redirect_to referer_or(admin_goods_reports_url(pid: @current_project.id)), notice: '报告已删除。' }
     end
   end
 
   def switch
     @report.show? ? @report.hidden! : @report.show!
-    redirect_to admin_goods_reports_url(pid: @project.id), notice:  @report.show? ? '报告已展示' : '报告已隐藏'
+    redirect_to admin_goods_reports_url(pid: @current_project.id), notice:  @report.show? ? '报告已展示' : '报告已隐藏'
   end
 
   private
-    def set_project
-      @project = Project.find(params[:pid])
-    end
-
     def set_report
       @report = ProjectReport.find(params[:id])
     end
