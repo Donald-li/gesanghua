6fd3ca719c1a9868bf3454201b11251236406fe3
王宪伟 <wangxw@wikiflyer.cn>
2018-04-28 15:45:56 +0800

    修改一些问题

--- app/uploaders/project_head_image_uploader.rb
+++ app/uploaders/project_head_image_uploader.rb
@@ -3,6 +3,7 @@ class ProjectHeadImageUploader < BaseUploader
   process :auto_orient
 
   version :tiny do
-    process :resize_to_fill => [220, 160]
+    process :resize_to_fill => [1140, 280]
   end
+
 end
