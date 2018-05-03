b7a0620fb3d69d0fbe8278aae3858d202134f66d
xu baiheng <18363993647@163.com>
2018-05-03 15:23:21 +0800

    任务二次申请和审核通知

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -29,6 +29,29 @@ class Notification < ApplicationRecord
   belongs_to :season, class_name: 'ProjectSeason', foreign_key: 'project_season_id', optional: true
   belongs_to :apply, class_name: 'ProjectSeasonApply', foreign_key: 'project_season_apply_id', optional: true
 
+  # def send_template_msg(url)
+  #   user = self.user
+  #   data = {
+  #       first: {
+  #           value: "看板消息",
+  #           color: "#173277"
+  #       },
+  #       keyword1: {
+  #           value: "看板：#{self.kanban.try(:title)}",
+  #           color: "#173177"
+  #       },
+  #       keyword2: {
+  #           value: self.created_at.to_s(:db),
+  #           color: "#274177"
+  #       },
+  #       remark: {
+  #           value: "#{self.from_user.try(:nickname)}#{self.plain_content}",
+  #           color: "#274377"
+  #       }
+  #   }
+  #   $wechat_client.send_template_msg(user.wechat_profile['openid'], Settings.wechat_template_message_id, url, "#173177", data)
+  # end
+
   private
   def set_assoc_attrs
     owner = self.owner
