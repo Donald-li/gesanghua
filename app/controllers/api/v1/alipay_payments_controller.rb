6ed256b79ba39fa6ed3449e78d9a0f4c24c2e584
王宪伟 <wangxw@wikiflyer.cn>
2018-05-04 17:14:36 +0800

    h5支付

--- app/controllers/api/v1/alipay_payments_controller.rb
+++ app/controllers/api/v1/alipay_payments_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::AlipayPaymentsController < Api::V1::BaseController
   skip_before_action :login?
 
   def h5
-    donation = DonateRecord.find params[:order_id]
+    donation = Donation.find_by(order_no: params[:order_no])
     pay_url = donation.alipay_prepay_h5
     api_success(data: {payUrl: pay_url})
   end
