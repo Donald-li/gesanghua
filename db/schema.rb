f8c7bb92d185b6a1b7cdf6b0ad90897e69e31089
董语 <dongyu@wikiflyer.cn>
2018-05-07 09:47:12 +0800

    pull代码

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504024521) do
+ActiveRecord::Schema.define(version: 20180504091621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1235,6 +1235,8 @@ ActiveRecord::Schema.define(version: 20180504024521) do
     t.string "whodunnit"
     t.text "object"
     t.datetime "created_at"
+    t.text "object_changes"
+    t.string "ip"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
