2ef34c636182f814c641f90a10454ba5e47f7ce9
董语 <dongyu@wikiflyer.cn>
2018-05-23 12:01:27 +0800

    pc志愿者查询

--- app/controllers/site/volunteers_controller.rb
+++ app/controllers/site/volunteers_controller.rb
@@ -1,7 +1,10 @@
 class Site::VolunteersController < Site::BaseController
 
   def index
-
+    @search = Volunteer.ransack(params[:q])
+    scope = @search.result
+    scope = scope.page(params[:page]).per(12)
+    @volunteers = scope.sorted
   end
 
 end
