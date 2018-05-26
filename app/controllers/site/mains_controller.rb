043a8f3f97a00e6778a1f58b3e1f932dd653a448
王宪伟 <wangxw@wikiflyer.cn>
2018-05-26 20:04:32 +0800

    fix a bug

--- app/controllers/site/mains_controller.rb
+++ app/controllers/site/mains_controller.rb
@@ -2,7 +2,7 @@ class Site::MainsController < Site::BaseController
 
   def show
     @adverts = Advert.pc_banner.show.sorted
-    @donate_records = DonateRecord.sorted
+    @donate_records = DonateRecord.sorted.includes(:donor).limit(20)
     @articles = Article.show.recommend.sorted.limit(6)
     @partners = Partner.show.sorted
   end
