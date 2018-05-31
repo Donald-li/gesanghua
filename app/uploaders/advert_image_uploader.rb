3a76532dcc5b94b2e0784fb63a8d1c5de3f50a05
xu baiheng <18363993647@163.com>
2018-05-31 20:02:01 +0800

    pc首页项目捐款广告位

--- app/uploaders/advert_image_uploader.rb
+++ app/uploaders/advert_image_uploader.rb
@@ -13,4 +13,8 @@ class AdvertImageUploader < BaseUploader
   version :big do #pc端首页banner
     process :resize_to_fill => [1140, 420]
   end
+
+  version :small do # pc捐助入口 372X686
+    process :resize_to_fill => [372, 686]
+  end
 end
