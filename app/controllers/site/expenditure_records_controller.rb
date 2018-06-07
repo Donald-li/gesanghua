2206d0cf201f22d1afb8859f1df4799b3a45bfcb
王宪伟 <wangxw@wikiflyer.cn>
2018-06-07 14:13:36 +0800

    几个页面的问题

--- app/controllers/site/expenditure_records_controller.rb
+++ app/controllers/site/expenditure_records_controller.rb
@@ -3,7 +3,7 @@ class Site::ExpenditureRecordsController < Site::BaseController
   def index
     @search = ExpenditureRecord.ransack(params[:q])
     scope = @search.result
-    @expenditure_records = scope.sorted
+    @expenditure_records = scope.includes(:expenditure_ledger, :secure_operator).sorted.page(params[:page])
   end
 
   def show
--- app/controllers/site/mains_controller.rb
+++ app/controllers/site/mains_controller.rb
@@ -2,7 +2,7 @@ class Site::MainsController < Site::BaseController
 
   def show
     @adverts = Advert.pc_banner.show.sorted
-    @donate_records = DonateRecord.sorted.includes(:donor).limit(20)
+    @donate_records = DonateRecord.sorted.includes(:donor, :owner, :child).limit(20)
     @articles = Article.show.recommend.sorted.limit(6)
     @donate_adverts = Advert.pc_donate_banner.show.sorted.limit(3)
     @partners = Partner.show.sorted
