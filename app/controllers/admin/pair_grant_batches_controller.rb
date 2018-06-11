61afe9307fb10b511d728770503fe269d3210013
董语 <dongyu@wikiflyer.cn>
2018-06-11 14:44:08 +0800

    发放批次增加发放时间

--- app/controllers/admin/pair_grant_batches_controller.rb
+++ app/controllers/admin/pair_grant_batches_controller.rb
@@ -33,6 +33,9 @@ class Admin::PairGrantBatchesController < Admin::BaseController
   def create
     @batch = GrantBatch.new(grant_params)
     if @batch.save
+      if @batch.grant_at.present?
+        @batch.grants.update(granted_at: @batch.grant_at)
+      end
       redirect_to referer_or(admin_pair_grant_batches_url), notice: '发放批次已创建。'
     else
       render :new
@@ -48,6 +51,9 @@ class Admin::PairGrantBatchesController < Admin::BaseController
 
     respond_to do |format|
       if @batch.update(grant_params)
+        if @batch.grant_at.present?
+          @batch.grants.update(granted_at: @batch.grant_at)
+        end
         format.html { redirect_to referer_or(admin_pair_grant_batches_url), notice: '发放批次已更新。' }
       else
         format.html { render :edit }
