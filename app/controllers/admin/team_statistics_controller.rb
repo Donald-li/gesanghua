7914bb13a955623888597d73549da575e49f292e
xu baiheng <18363993647@163.com>
2018-06-12 15:09:49 +0800

    团队按年查询

--- app/controllers/admin/team_statistics_controller.rb
+++ app/controllers/admin/team_statistics_controller.rb
@@ -6,19 +6,13 @@ class Admin::TeamStatisticsController < Admin::BaseController
     @income_statistics = IncomeRecord.all
     @income_statistics = @income_statistics.where("income_time > ?", params[:time_start]) if params[:time_start].present?
     @income_statistics = @income_statistics.where("income_time < ?", params[:time_end].end_of_day) if params[:time_end].present?
-    @income_statistics = @income_statistics.where("income_time > ?", Time.now.beginning_of_day) if params[:fix_time] == 'day'
-    @income_statistics = @income_statistics.where("income_time > ?", Time.now.beginning_of_day - 7.day) if params[:fix_time] == 'week'
-    @income_statistics = @income_statistics.where("income_time > ?", Time.now.beginning_of_day - 1.month) if params[:fix_time] == 'month'
-    @income_statistics = @income_statistics.where("income_time > ?", Time.now.beginning_of_day - 1.year) if params[:fix_time] == 'year'
+    @income_statistics = @income_statistics.where("income_time > ?", Time.mktime(params[:fix_year])) if params[:fix_year].present?
     @income_statistics = @income_statistics.select("team_id, sum(amount) as amount").group(:team_id)
 
     @promote_statistics = IncomeRecord.where.not(promoter_id: nil)
     @promote_statistics = @promote_statistics.where("income_time > ?", params[:time_start]) if params[:time_start].present?
     @promote_statistics = @promote_statistics.where("income_time < ?", params[:time_end].end_of_day) if params[:time_end].present?
-    @promote_statistics = @promote_statistics.where("income_time > ?", Time.now.beginning_of_day) if params[:fix_time] == 'day'
-    @promote_statistics = @promote_statistics.where("income_time > ?", Time.now.beginning_of_day - 7.day) if params[:fix_time] == 'week'
-    @promote_statistics = @promote_statistics.where("income_time > ?", Time.now.beginning_of_day - 1.month) if params[:fix_time] == 'month'
-    @promote_statistics = @promote_statistics.where("income_time > ?", Time.now.beginning_of_day - 1.year) if params[:fix_time] == 'year'
+    @promote_statistics = @promote_statistics.where("income_time > ?", Time.mktime(params[:fix_year])) if params[:fix_year].present?
     @promote_statistics = @promote_statistics.select("team_id, sum(amount) as amount").group(:team_id)
   end
 
