8cddbc5a256a8b43a0d1de9dee9a301c487e465e
xu baiheng <18363993647@163.com>
2018-06-15 19:04:57 +0800

    累计捐助金额统计

--- app/controllers/admin/data_statistics_controller.rb
+++ app/controllers/admin/data_statistics_controller.rb
@@ -5,7 +5,7 @@ class Admin::DataStatisticsController < Admin::BaseController
     @user_count = User.enable.count
     @volunteer_count = Volunteer.enable.count
     @donate_record = DonateRecord.count
-    @income_count = IncomeRecord.sum(:amount)
+    @income_count = IncomeRecord.can_count.sum(:amount)
 
   end
 
