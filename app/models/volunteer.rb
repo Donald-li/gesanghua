9eed3502beb6cf8ef9d302e3d348195205261733
xu baiheng <18363993647@163.com>
2018-06-25 14:31:32 +0800

    每次保存都判断性别

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -41,7 +41,7 @@ class Volunteer < ApplicationRecord
   has_paper_trail only: [:level, :duration, :user_id, :job_state, :state, :kind, :approve_state, :approve_time, :approve_remark, :volunteer_no, :volunteer_apply_no,
      :internship_state, :describe, :phone, :workstation, :leave_reason, :name, :id_card, :province, :city, :district, :address, :gender, :source, :experience]
 
-  after_create :distinguish_gender
+  after_save :distinguish_gender
 
   belongs_to :user, optional: true
   has_many :task_volunteers, dependent: :destroy
