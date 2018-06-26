3c8e68cb2d4a5e407bc93e5d9f7f8875a8bed6b6
xu baiheng <18363993647@163.com>
2018-06-26 11:15:53 +0800

    我的结对

--- app/controllers/api/v1/account/pair_children_controller.rb
+++ app/controllers/api/v1/account/pair_children_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Account::PairChildrenController < Api::V1::BaseController
 
   def index
     apply_child_ids = current_user.donate_records.visible.pluck(:project_season_apply_child_id)
-    apply_child_ids = apply_child_ids.push(ProjectSeasonApplyChild.where(priority_id: current_user.id).pluck(:id)).uniq
+    apply_child_ids = apply_child_ids.push(ProjectSeasonApplyChild.where(priority_id: current_user.id).pluck(:id)).flatten.uniq
     children = ProjectSeasonApplyChild.where(id: apply_child_ids).sorted.page(params[:page]).per(params[:per])
     api_success(data: {children: children.map { |r| r.donate_children_builder(current_user) }, pagination: json_pagination(children)})
   end
