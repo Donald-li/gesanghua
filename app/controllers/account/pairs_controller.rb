b10212e6146b2d843adb6e791b524a78df57f734
xu baiheng <18363993647@163.com>
2018-07-05 13:29:07 +0800

    我的结对统一微信和pc查询

--- app/controllers/account/pairs_controller.rb
+++ app/controllers/account/pairs_controller.rb
@@ -2,7 +2,7 @@ class Account::PairsController < Account::BaseController
 
   def index
     apply_child_ids = current_user.donate_records.visible.pluck(:project_season_apply_child_id)
-    apply_child_ids = apply_child_ids.push(ProjectSeasonApplyChild.where(priority_id: current_user.id).pluck(:id)).uniq
+    apply_child_ids = apply_child_ids.push(ProjectSeasonApplyChild.where(priority_id: current_user.id).pluck(:id)).flatten.uniq
     scope = ProjectSeasonApplyChild.where(id: apply_child_ids).sorted
     @children = scope.page(params[:page]).per(4)
   end
