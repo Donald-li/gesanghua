93a1e222bb1d55a5e9d1ca2b704a41bf3c03b750
xu baiheng <18363993647@163.com>
2018-07-13 11:57:56 +0800

    不验证学校联系电话的格式

--- app/models/school.rb
+++ app/models/school.rb
@@ -61,7 +61,7 @@ class School < ApplicationRecord
   has_many :apply_camps, class_name: 'ProjectSeasonApplyCamp', dependent: :restrict_with_error
 
   validates :name, :province, :city, :district, presence: true
-  validates :contact_phone, mobile: true
+  # validates :contact_phone, mobile: true
   validates :contact_id_card, shenfenzheng_no: true
 
   enum state: {enable: 1, disable: 2} # 状态：1:启用 2:禁用
