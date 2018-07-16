48972909f9cc4c131363560dcd0baac16de6834e
xu baiheng <18363993647@163.com>
2018-07-16 15:16:35 +0800

    一二级筛选分开

--- app/controllers/admin/finance_statistics_controller.rb
+++ app/controllers/admin/finance_statistics_controller.rb
@@ -4,17 +4,19 @@ class Admin::FinanceStatisticsController < Admin::BaseController
   def show
     params[:time_start] ||= Time.now.beginning_of_month.strftime("%Y-%m-%d")
     params[:time_end] ||= Time.now.strftime("%Y-%m-%d")
-    income_records = IncomeRecord.left_joins(:agent).select("income_no as finance_no, title, fund_id, income_time as finance_time, amount as income_amount, 0 as expend_amount, income_source_id, users.name as operator, income_records.remark")
+    income_records = IncomeRecord.joins(:fund).left_joins(:agent).select("income_no as finance_no, title, fund_id, income_time as finance_time, amount as income_amount, 0 as expend_amount, income_source_id, users.name as operator, income_records.remark")
     income_records = income_records.where("income_time >= ?", Time.parse(params[:time_start])) if params[:time_start].present?
     income_records = income_records.where("income_time <= ?", Time.parse(params[:time_end]).end_of_day) if params[:time_end].present?
+    income_records = income_records.where("funds.fund_category_id = ?", params[:fund_category_id]) if params[:fund_category_id].present?
     income_records = income_records.where(fund_id: params[:fund_id]) if params[:fund_id].present?
     income_records = income_records.where(income_source_id: params[:income_source_id]) if params[:income_source_id].present?
     income_records = income_records.where("title like '%#{params[:keyword]}%' or income_records.remark like '%#{params[:keyword]}%'") if params[:keyword].present?
 
 
-    expend_records = ExpenditureRecord.select("expend_no as finance_no, name as title, fund_id, expended_at as finance_time, 0 as income_amount, amount as expend_amount, income_source_id, operator, expenditure_records.remark")
+    expend_records = ExpenditureRecord.joins(:fund).select("expend_no as finance_no, expenditure_records.name as title, fund_id, expended_at as finance_time, 0 as income_amount, amount as expend_amount, income_source_id, operator, expenditure_records.remark")
     expend_records = expend_records.where("expended_at >= ?", Time.parse(params[:time_start])) if params[:time_start].present?
     expend_records = expend_records.where("expended_at <= ?", Time.parse(params[:time_end]).end_of_day) if params[:time_end].present?
+    expend_records = expend_records.where("funds.fund_category_id = ?", params[:fund_category_id]) if params[:fund_category_id].present?
     expend_records = expend_records.where(fund_id: params[:fund_id]) if params[:fund_id].present?
     expend_records = expend_records.where(income_source_id: params[:income_source_id]) if params[:income_source_id].present?
     expend_records = expend_records.where("name like '%#{params[:keyword]}%' or expenditure_records.remark like '%#{params[:keyword]}%'") if params[:keyword].present?
