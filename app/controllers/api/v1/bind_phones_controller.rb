8999b53a894c246a62668da3f26dbcd4dff1db76
xu baiheng <18363993647@163.com>
2018-07-18 11:13:51 +0800

    修改手机号也走绑定流程

--- app/controllers/api/v1/bind_phones_controller.rb
+++ app/controllers/api/v1/bind_phones_controller.rb
@@ -51,12 +51,39 @@ class Api::V1::BindPhonesController < Api::V1::BaseController
 
   def update
     if SmsCode.valid_code?(mobile: params[:mobile], code: params[:code], kind: 'login', write_verified: true)
-      current_user.phone = params[:mobile]
-      if current_user.save
-        current_user.bind_user_roles
-        api_success(message: '绑定成功', data: {state: true})
+      if User.find_by(phone: params[:mobile]).present?
+        user = User.find_by(phone: params[:mobile])
+        if user.unactived? && user.manager_id.present?
+          if user.offline_user_activation(user.phone, current_user)
+            set_current_user(user)
+            if params[:password].present?
+              user.password = params[:password]
+              user.save
+            end
+            api_success(message: '绑定成功', data: {state: true})
+          else
+            api_success(message: '绑定失败，请联系微信号gshkf001', data: {state: false})
+          end
+        elsif !user.openid.present?
+          User.combine_user(params[:mobile], current_user)
+          set_current_user(user)
+          user.phone = params[:mobile]
+          if params[:password].present?
+            user.password = params[:password]
+          end
+          user.save
+          api_success(message: '绑定成功', data: {state: true})
+        else
+          api_success(message: '绑定失败，手机号已占用，请联系微信号gshkf001', data: {state: false})
+        end
       else
-        api_success(message: '绑定失败，手机号已占用，请联系微信号gshkf001', data: {state: false})
+        current_user.phone = params[:mobile]
+        if current_user.save
+          current_user.bind_user_roles
+          api_success(message: '绑定成功', data: {state: true})
+        else
+          api_success(message: '绑定失败，手机号已占用，请联系微信号gshkf001', data: {state: false})
+        end
       end
     else
       api_error(message: '验证码错误')
