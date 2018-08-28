ea85090a5b9b7a92108ed4539a85e1aa66245939
xu baiheng <18363993647@163.com>
2018-08-28 10:06:10 +0800

    小优化

--- app/controllers/account/pairs_controller.rb
+++ app/controllers/account/pairs_controller.rb
@@ -3,7 +3,7 @@ class Account::PairsController < Account::BaseController
   def index
     apply_child_ids = current_user.donate_records.visible.pluck(:project_season_apply_child_id)
     apply_child_ids = apply_child_ids.push(ProjectSeasonApplyChild.where(priority_id: current_user.id).pluck(:id)).flatten.uniq
-    @search = ProjectSeasonApplyChild.where(id: apply_child_ids).sorted.ransack(params[:q])
+    @search = ProjectSeasonApplyChild.joins(:gsh_child).where(id: apply_child_ids).sorted.ransack(params[:q])
     scope = @search.result
     @children = scope.page(params[:page]).per(4)
   end
