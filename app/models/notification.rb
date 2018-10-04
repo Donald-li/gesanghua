eeafdbe57962e508acddd88e6e3977d72d89308b
xu baiheng <18363993647@163.com>
2018-10-04 11:50:31 +0800

    待优化

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -151,7 +151,7 @@ class Notification < ApplicationRecord
             color: "#274377"
         }
     }
-    $client.send_template_msg(self.user.openid, template_id, url, "#173177", data) if self.user.openid.present?
+    $client.send_template_msg(self.user.openid, template_id, url, "#173177", data) # if self.user.openid.present?
   end
 
 
