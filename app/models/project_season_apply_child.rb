7952833a63512f8e846b04f0fd07b48e0a4f6a92
xu baiheng <18363993647@163.com>
2018-10-15 14:19:13 +0800

    方法优化

--- app/models/project_season_apply_child.rb
+++ app/models/project_season_apply_child.rb
@@ -427,7 +427,7 @@ class ProjectSeasonApplyChild < ApplicationRecord
   # 是否被用户认捐？
   def donate_by_user?(user)
     return false unless user
-    self.priority_user == user || self.priority_user == user.manager || user.offline_users.unactived.pluck(:id).include?(self.priority_id) || donate_grants_by_user(user).exists?
+    self.priority_user == user || (self.priority_user.present? && self.priority_user == user.manager) || user.offline_users.unactived.pluck(:id).include?(self.priority_id) || donate_grants_by_user(user).exists?
   end
 
   # 用户捐助的学期
