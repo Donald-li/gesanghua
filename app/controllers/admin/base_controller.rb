935eccd8a0faa54680412eb90c5ab7d3e0552180
xu baiheng <18363993647@163.com>
2018-11-02 10:49:43 +0800

    debug

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -32,7 +32,7 @@ class Admin::BaseController < ManagementBaseController
     can_entrance = (@current_entrance_cards[request.path_parameters[:controller]][request.path_parameters[:action]].compact.uniq & current_user.roles).present?
     can_project = session[:goods_project_id].present? ? current_user.project_ids.include?(session[:goods_project_id]) : true
     unless can_entrance && can_project
-      redirect_to referer_or(admin_main_path), alert: '您没有权限'
+      redirect_to admin_main_path, alert: '您没有权限'
     end
   end
 
