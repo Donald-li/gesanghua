69b945f8c7b62322989e715f4aacba8aa73e69bc
董语 <dongyu@wikiflyer.cn>
2019-02-01 15:48:17 +0800

    我的勋章-报错

--- app/models/badge_level.rb
+++ app/models/badge_level.rb
@@ -54,12 +54,15 @@ class BadgeLevel < ApplicationRecord
     elsif kind == 'volunteer_age'
       value = owner.volunteer.try(:volunteer_age).to_i
       self.level(kind, value.to_i)
-    else
+    elsif (kind == 'society_team' || kind == 'college_team') && owner.team.present?
       if kind == 'society_team'
         value = owner.team.society? ? owner.team.try(:total_donate_amount) : 0
       else
         value = owner.team.college? ? owner.team.try(:total_donate_amount) : 0
       end
+      self.level(kind, value.to_i)
+    else
+      value = 0
       self.level(kind, value.to_i)
     end
 
