76eaef7961b09079e4c730066eb5d2a4f64acb28
xu baiheng <18363993647@163.com>
2019-09-18 14:33:01 +0800

    图形验证+短信

--- app/controllers/api/v1/common/sms_codes_controller.rb
+++ app/controllers/api/v1/common/sms_codes_controller.rb
@@ -6,6 +6,9 @@ class Api::V1::Common::SmsCodesController < Api::V1::BaseController
     if user.present? && current_user != user
       type = user.unactived? ? 'offline_active' : 'user_combine'
     end
+    unless verify_rucaptcha?
+      api_error(message: '请输入图形验证码') and return
+    end
     code = SmsCode.send_code(params[:mobile], params[:kind], ip: request.remote_ip)
     if code.valid?
       api_success(message: (Settings.send_sms ? nil : code.code), data: {type: type})
