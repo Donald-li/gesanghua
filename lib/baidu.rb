0017516fece88faf4e72de937ffec6704ec4f201
xubh <xubh@wikiflyer.cn>
2020-09-18 19:03:05 +0800

    baidu sms

--- lib/baidu.rb
+++ lib/baidu.rb
@@ -5,9 +5,9 @@ require "net/http"
 module Baidu
   ACCESS_ID = '18bf2b9a12834efebc1643bdf4efb933'
   SECRET_KEY = '736cf755dc6142768dcd18052c7c7520'
-  SMS_HOST = 'sms.bj.baidubce.com'
-  SMS_INVOKE_ID = '9zwgVSqL-G3ux-pfry'
-  SMS_TEMPLATE_CODE = 'smsTpl:e7476122a1c24e37b3b0de19d04ae900'
+  SMS_HOST = 'smsv3.bj.baidubce.com'
+  SMS_SIGN_ID = 'sms-sign-DmojLW91016'
+  SMS_TEMPLATE_CODE = 'sms-tmpl-FhwtyC36444'
 
   # 签名的算法
   def self.auth_str(method: 'GET', url: '', timestamp: Time.now.utc.strftime('%FT%TZ'), headers: {})
@@ -28,20 +28,20 @@ module Baidu
     "#{signing_str}/#{signed_headers}/#{signature}"
   end
 
-  def self.send_sms(code: '', mobile: '', type: :signup)
-    templates = {
-      find_password: 'smsTpl:e7476122a1c24e37b3b0de19d04ae903',
-    }
+  def self.send_sms(code: '', mobile: '', type: 'signup')
+    if type == 'find_password'
+      template = 'sms-tmpl-mCTxNs19665'
+    else
+      template  = SMS_TEMPLATE_CODE
+    end
 
-    template  = templates[type.to_sym] || 'smsTpl:e7476122a1c24e37b3b0de19d04ae901' # 通用的
-
-    path = '/bce/v2/message'
+    path = '/api/v3/sendSms'
     timestamp = Time.now.utc.strftime('%FT%TZ')
     method = 'POST'
     body = {
-      invokeId: SMS_INVOKE_ID,
-      phoneNumber: mobile,
-      templateCode: template,
+      signatureId: SMS_SIGN_ID,
+      mobile: mobile,
+      template: template,
       contentVar: {code: code.to_s}
     }.to_json
     signed_content = Digest::SHA256.hexdigest(body)
@@ -61,8 +61,8 @@ module Baidu
   end
 
   def self.send_test
-    host = 'sms.bj.baidubce.com'
-    path = '/v1/quota'
+    host = 'smsv3.bj.baidubce.com'
+    path = '/api/v3/sendSms'
     # host = 'bcc.bj.baidubce.com'
     # path = '/v2/volume/v-NZP6K01R'
 
