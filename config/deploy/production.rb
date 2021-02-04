0677ad970ace158881a59d220124d3dade0ac27e
wikideMacBook <weiyilin@wikifly.com>
2021-02-04 10:52:23 +0800

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :domain, 'www@180.97.219.87'
+set :domain, 'root@180.97.219.87'
 set :deploy_to, '/data/www/gesanghua'
 set :repository, 'http://gitlab+deploy-token-45:-qbsfkRvNzYoUwxDCTDj@gitlab.wikiflyer.cn/wikiflyer/gesanghua.git'
 set :branch, 'master'
